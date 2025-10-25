2b9449e83b8fb74dbdd26b1825d93d5f1f305f9c
yma2023 <apple@appurunoMac-mini.local>
2025-10-25 14:54:11 +0900

    New version

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useAuthStore } from './stores/authStore';
 import { LandingPage } from './components/LandingPage';
 import { Dashboard } from './components/Dashboard';
 import { MindMapCanvas } from './components/MindMapCanvas';
-import { AdvancedMindMap } from './components/AdvancedMindMap';
 import '@xyflow/react/dist/style.css';
 
 const MindMapApp: React.FC = () => {
@@ -32,8 +31,6 @@ function App() {
         {/* Public routes - no authentication required */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/mindmap" element={<MindMapApp />} />
-        <Route path="/interactive" element={<AdvancedMindMap />} />
-        <Route path="/advanced-mindmap" element={<AdvancedMindMap />} />
         
         {/* Default redirect */}
         <Route path="/home" element={<Navigate to="/dashboard" replace />} />
